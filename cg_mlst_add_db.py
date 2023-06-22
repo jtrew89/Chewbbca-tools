0f67b12cb4b7206d43d26d33bc6f8bf46a86ad60
Jahcub Trew <jahcubtrew@ranch-510.int.sce.network>
2023-06-22 15:41:08 +0100

    Update to cg_mlst_add_db.py

--- cg_mlst_add_db.py
+++ cg_mlst_add_db.py
@@ -6,44 +6,45 @@ import os
 import argparse
 import time
 
-##Create arguments
-parser = argparse.ArgumentParser(description="Script written to add a new results_alleles.tsv allele profile to the Salmonella enterica database (sent_curr_db.csv)")
-parser.add_argument(
-	'-id', '--input_directory',
-	dest='in_dir',
-	help="Directory where results_alleles.tsv is kept",
-	required=True
-		)
-parser.add_argument(
-	'-sdb','--strain_db',
-	dest='str_db',
-	help="Strain db to update and path to",
-	required=True
-		)
-parser.add_argument(
-	'-dbd','--db_dir',
-	dest='db_dir',
-	help="Directory with strain db",
-	required=True
-		)
+def main(args):
+	##Variables used in script
+	date = time.strftime('%Y%m%d')
+	in_file = args.in_dir+'results_alleles.tsv'
 
-args = parser.parse_args()
+	##Load database and allele profile to be added
+	in_alle = pd.read_table(in_file)
+	db_alle = pd.read_csv(args.str_db)
 
-##Variables used in script
-date = time.strftime('%Y%m%d')
-in_file = args.in_dir+'results_alleles.tsv'
+	##Concat allele profiles for new db
+	new_db = pd.concat([in_alle,db_alle],ignore_index=True)
 
-##Load database and allele profile to be added
-in_alle = pd.read_table(in_file)
-db_alle = pd.read_csv(args.str_db)
-
-##Concat allele profiles for new db
-new_db = pd.concat([in_alle,db_alle],ignore_index=True)
-
-##Save new db
-new_db.to_csv(db_dir+'curr_db_'+date+'csv')
+	##Save new db
+	new_db.to_csv(db_dir+'curr_db_'+date+'csv')
 
 if __name__ == '__main__':
-	
+	##Create arguments
+	parser = argparse.ArgumentParser(description="Script written to add a new results_alleles.tsv allele profile to the Salmonella enterica database (sent_curr_db.csv)")
+	parser.add_argument(
+		'-id', '--input_directory',
+		dest='in_dir',
+		help="Directory where results_alleles.tsv is kept",
+		required=True
+			)
+	parser.add_argument(
+		'-sdb','--strain_db',
+		dest='str_db',
+		help="Strain db to update and path to",
+		required=True
+			)
+	parser.add_argument(
+		'-dbd','--db_dir',
+		dest='db_dir',
+		help="Directory with strain db",
+		required=True
+			)
 
+	args = parser.parse_args()
 
+	##Convert the argparse.Namespace to a dictionary: vars(args)
+	main(vars(args))
+	sys.exit(0)
